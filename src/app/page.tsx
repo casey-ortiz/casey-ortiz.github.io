2822e141defc04b227e1cd8f8bb694f9cdc90541
Phillip Wei <wei.ph@northeastern.edu>
2024-09-10 21:18:21 -0700

    Improved homepage responsive design + added The Seasons font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import Image from "next/image"
-import logo from "../../public/logo.png"
+import logo from "../../public/images/logo.png"
 export default function Home() {
   return (
     <div 
-      className="grid grid-rows-10 h-full bg-contain bg-center bg-no-repeat"
+      className="grid grid-rows-10` h-full bg-cover bg-center bg-no-repeat"
       style={{
-        backgroundImage: "url('/homepage.jpg')",
+        backgroundImage: "url('/images/homepage.jpg')",
     }}>
-      <div className="row-span-2 w-9/12 mx-auto my-0">
+      <div 
+        className="row-span-1 mx-auto my-0"
+        style={{
+          width: "75vw",
+          height: "3vh",}}
+      >
         <Image 
             src={logo}
+            width={150}
             alt="Logo for Casey Ortiz"
         />
       </div>
+      <hr style={{width: "75vw", borderColor: "#71bae7", margin: "auto"}}></hr>
       <div className="row-span-8 grid justify-center">
         <h1 
           className="sm:text-5xl md:text-6xl lg:text-7xl"
