5c0afeb4291efd4c0f48f2be9eb386861762b2be
Phillip Wei <wei.ph@northeastern.edu>
2024-09-14 15:24:18 -0700

    Added navbar + improved homepage responsiveness again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,32 @@
 import Image from "next/image"
 import logo from "../../public/images/logo.png"
+
 export default function Home() {
   return (
-    <div 
-      className="grid grid-rows-10` h-full bg-cover bg-center bg-no-repeat"
+    <div
+      className="h-full bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: "url('/images/homepage.jpg')",
     }}>
       <div 
-        className="hidden sm:block sm:row-span-1 mx-auto my-0"
+        className="hidden md:block sm:py-5 mx-auto my-0"
         style={{
-          width: "80vw",
-          height: "3vh",}}
+          width: "80vw"
+        }}
       >
         <Image 
-            src={logo}
-            width={150}
-            alt="Logo for Casey Ortiz"
+          src={logo}
+          width={80}
+          height={0}
+          className="mt-10"
+          style={{height: "auto"}}
+          alt="Logo for Casey Ortiz"
         />
       </div>
-      <hr className="hidden sm:block" style={{width: "75vw", borderColor: "#71bae7", margin: "auto"}}></hr>
-      <div className="row-span-10 sm:row-span-8 grid justify-center pt-28 sm:pt-0">
-        <h1 
-          className="text-5xl md:text-6xl lg:text-7xl"
+      <hr className="hidden md:block" style={{width: "85vw", borderColor: "#71bae7", margin: "auto"}}></hr>
+      <div className="pt-28 md:pt-5 grid justify-center align-middle">
+        <h1
+          className="text-6xl sm:text-7xl md:text-8xl"
           style={{color: "#71bae7"}}>
             CASEY ORTIZ
         </h1>
