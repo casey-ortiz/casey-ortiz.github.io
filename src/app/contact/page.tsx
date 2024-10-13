f9b8bef0b75e29940fae1feedda888415f95b5fc
Phillip Wei <wei.ph@northeastern.edu>
2024-10-13 15:41:57 -0700

    Updated contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,22 @@
+import SocialIcons from "../socials/social-icons";
+
 export default function Contact() {
   return(
-    <div className="flex justify-center items-center h-screen pt-10">
-      <p className="text-3xl">Coming soon!</p>
+    <div className="flex flex-col items-center h-screen pt-5">
+      <div 
+          className="grid justify-center m-auto md:block md:justify-normal"
+          style={{ width: "85vw" }}
+        >
+          <h1 className="text-5xl lg:text-6xl" style={{color: "#FFFFFF"}}>Contact</h1>
+      </div>
+      <hr className="block m-auto" style={{width: "85vw"}}></hr>
+      <iframe 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfM48CNN4k5GfEyMmq3O18Lbry50OEKX6-ihT9S_yjefL_XTw/viewform?embedded=true"
+        style={{ width: "50vw", height: "60vh" }}
+      >
+        Loading…
+      </iframe>
+      <SocialIcons />
     </div>
   );
 }
